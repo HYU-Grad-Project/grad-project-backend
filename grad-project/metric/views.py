2bb4ec920c7ff66dce653349702da3a00735e610
MinJooooon <wns21243@gmail.com>
2023-09-04 18:35:33 +0900

    /metric/monitor/{metric_id} API 추가

--- grad-project/metric/views.py
+++ grad-project/metric/views.py
@@ -53,6 +53,7 @@ class MetricViewSet(ModelViewSet):
         'list': MetricListReadSerializer,
         'read': MetricSerializer,
         'list_category': MetricListReadSerializer,
+        'monitor': MetricSerializer,
         'getget': MetricSerializer,
         'modify_replicaSet': MetricSerializer,
     }
@@ -85,7 +86,43 @@ class MetricViewSet(ModelViewSet):
             metrics = Metric.objects.filter(category_id=category_id)
         serializer = self.get_serializer(metrics, many=True)
         return Response(serializer.data)
-            
+    
+    @action(methods=['post'], detail=False)
+    def monitor(self, request):
+        metric_id = int(request.GET.get('metric_id', 0))
+        http = httplib2.Http()
+        metric = Metric.objects.get(id=metric_id)
+        metric_name = metric.name if metric is not None else ''
+        
+        # Cluster 내의 prometheus pod의 ip 주소를 입력해야함
+        prometheus_ip = "http://127.0.0.1:9090"
+        url = prometheus_ip + "/api/v1/query?query=" + metric_name
+        response, response_body = http.request(url, method="GET", 
+                                         headers={'Content-Type': 'application/json;'})
+        response_str = response_body.decode('utf-8')
+        response_dict = None
+        error = None
+        try:
+            response_dict = json.loads(response_str)
+        except ValueError:
+            return JsonResponse({"error": "오류가 발생했습니다.[0]"}, status=HTTP_400_BAD_REQUEST)
+        except TypeError:
+            return JsonResponse({"error": "오류가 발생했습니다.[1]"}, status=HTTP_400_BAD_REQUEST)
+        if error is None and response_dict['status'] == 'success' and response_dict['data']['result']:
+            pod_value_list = []
+            result_list = response_dict['data']['result']
+            for result in result_list:
+                try:
+                    pod_name = result['metric']['pod']
+                    value = result['value'][1]
+                except KeyError:
+                    return JsonResponse({"error": "오류가 발생했습니다.[2]"}, status=HTTP_400_BAD_REQUEST)
+                pod_value = {"pod_name": pod_name, "value": value}
+                pod_value_list.append(pod_value)
+            return Response(pod_value_list)
+        else:
+            return JsonResponse({"error": "오류가 발생했습니다.[3]"}, status=HTTP_400_BAD_REQUEST)
+
 
     @action(methods=['post'], detail=False)
     def getget(self, request):
@@ -97,28 +134,6 @@ class MetricViewSet(ModelViewSet):
         metric.save()
         
         return JsonResponse({"msg": metric_desc})
-        # http = httplib2.Http()
-        # metric_name = ''
-        # try:
-        #     metric_name = request.data['query']
-        # except KeyError:
-        #     metric_name = ''
-        # url = "http://127.0.0.1:9090/api/v1/query?query=" + metric_name
-        # response, response_body = http.request(url, method="GET", 
-        #                                  headers={'Content-Type': 'application/json;'})
-        # response_str = response_body.decode('utf-8')
-        # response_dict = None
-        # error = None
-        # try:
-        #     response_dict = json.loads(response_str)
-        # except ValueError:
-        #     error = '오류가 발생했습니다.'
-        # except TypeError:
-        #     error = '오류가 발생했습니다.'
-        # if error is None and response_dict['status'] == 'success' and response_dict['data']['result']:
-        #     return Response(response_dict)
-        # else:
-        #     return JsonResponse({"error": "해당 query에 대한 결과값을 읽을 수 없습니다."}, status=HTTP_400_BAD_REQUEST)
         
     @action(methods=['post'], detail=False)
     def modify_replicaSet(self, request):
