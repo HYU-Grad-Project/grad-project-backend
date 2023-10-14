acdc342ed1402d5820b1302e7baa0be32d9629ff
MinJooooon <wns21243@gmail.com>
2023-10-14 13:59:41 +0900

    Unresolved Alert count 10 이상만 체크하던 것을 모두 체크하도록 변경

--- grad-project/alert/functions.py
+++ grad-project/alert/functions.py
@@ -9,7 +9,7 @@ import os
 
 # 호출 횟수가 10회 이상인 Unresolved Alerts에 대한 확인
 def resolve_alerts():
-    alerts = Alert.objects.filter(resolved=False, count__gte = 10)
+    alerts = Alert.objects.filter(resolved=False)
     for alert in alerts:
         rule = alert.rule
         pod_name = alert.pod_name
