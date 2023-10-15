0cd1482222caf1b2e5aeaff4d7695fbf570f3920
MinJooooon <wns21243@gmail.com>
2023-10-15 17:36:41 +0900

    RuleMetric 테이블 생성 후 관련 API 수정

--- grad-project/alert/models.py
+++ grad-project/alert/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from metric.models import Metric
 
 # Create your models here.
 class Rule(models.Model):
@@ -6,7 +7,7 @@ class Rule(models.Model):
     name = models.CharField('이름', db_column='name', max_length=100)
     query = models.CharField('표현식', db_column='query', max_length=3000)
     operator = models.CharField('연산자', db_column='operator', max_length=30)
-    threshold = models.IntegerField('임계값', db_column='threshold')
+    threshold = models.FloatField('임계값', db_column='threshold')
     severity = models.CharField('심각도', db_column='severity', max_length=50)
     description = models.CharField('설명', db_column='description', max_length=3000)
     relevant_key_name = models.CharField('CRD key 값', db_column='relevant_key_name', max_length=100)
@@ -20,6 +21,17 @@ class Rule(models.Model):
     def __str__(self):
         return self.name.__str__()
 
+class RuleMetric(models.Model):
+    id = models.AutoField(db_column='id', primary_key=True)
+    rule = models.ForeignKey(Rule, models.DO_NOTHING, help_text = '룰', db_column='rule_id', verbose_name='룰')
+    metric = models.ForeignKey(Metric, models.DO_NOTHING, help_text = '메트릭', db_column='metric_id', verbose_name='메트릭')
+    
+    class Meta:
+        managed = False
+        db_table = 'rule_metric'
+        verbose_name='룰_알럿'
+        verbose_name_plural='룰 알럿'
+    
 class Alert(models.Model):
     id = models.AutoField(db_column='id', primary_key=True)
     rule = models.ForeignKey(Rule, models.DO_NOTHING, help_text = '룰', db_column='rule_id', verbose_name='룰')
--- grad-project/alert/serializer.py
+++ grad-project/alert/serializer.py
@@ -1,20 +1,37 @@
 from rest_framework import serializers
-from .models import Rule, Alert
+from .models import Rule, Alert, RuleMetric
 
-class AlertSerializer(serializers.ModelSerializer):
-    rule_name = serializers.SerializerMethodField(help_text='룰 이름')
+class RuleMetricSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(help_text='id')
+    category_id = serializers.SerializerMethodField(help_text='카테고리')
+    name = serializers.SerializerMethodField(help_text='이름')
     class Meta:
-        model = Alert
-        fields = ['id', 'rule_name', 'fingerprint', 'pod_name', 'created_at', 'resolved_at', 'resolved', 'count']
-        
-    def get_rule_name(self, instance):
-        return instance.rule.name
+        model = RuleMetric
+        fields = ['id', 'category_id', 'name']
     
+    def get_id(self, instance):
+        return instance.metric.id
+    def get_category_id(self, instance):
+        return instance.metric.category.id
+    def get_name(self, instance):
+        return instance.metric.name
+
 class RuleSerializer(serializers.ModelSerializer):
     count = serializers.SerializerMethodField(help_text='알럿 개수')
+    metrics = serializers.SerializerMethodField(help_text='메트릭')
     class Meta:
         model = Rule
-        fields = ['id', 'name', 'query', 'operator', 'threshold', 'severity', 'description', 'count', 'relevant_key_name']
+        fields = ['id', 'name', 'query', 'operator', 'threshold', 'severity', 'description', 'count', 'relevant_key_name', 'metrics']
         
     def get_count(self, instance):
         return Alert.objects.filter(rule__id = instance.id).count()
+
+    def get_metrics(self, instance):
+        rule_metric_instances = RuleMetric.objects.filter(rule__id = instance.id)
+        return RuleMetricSerializer(rule_metric_instances, many = True).data
+    
+class AlertSerializer(serializers.ModelSerializer):
+    rule = RuleSerializer(many = False)
+    class Meta:
+        model = Alert
+        fields = ['id', 'fingerprint', 'pod_name', 'created_at', 'resolved_at', 'resolved', 'count', 'rule']
